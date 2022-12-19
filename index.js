533a294d269b431219e1ac2d6da459d696a4d8ac
Vinod Kotagiri <vinodkotagiri@icloud.com>
2022-12-19 12:06:11 +0530

    Final Code

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const express = require('express')
 const colors = require('colors')
+const cors = require('cors')
 require('dotenv').config()
 const connectDB = require('./config/db')
 const { graphqlHTTP } = require('express-graphql')
 const schema = require('./schema/schema')
 const PORT = process.env.PORT || 5000
 const app = express()
+app.use(cors())
 connectDB()
 app.use('/graphql', graphqlHTTP({ schema, graphiql: process.env.NODE_ENV === 'development' }))
 app.listen(PORT, console.log('Server listening on port: ' + PORT))
